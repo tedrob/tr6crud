6f4b2d6318615f6519853ea3840bc6d5f6643900
Ted Robinson <tfqbtd@aol.com>
2018-06-18 09:56:10 -0500

    made updates

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-let express     = require('express'),
-    path        = require('path'),
-    bodyParser  = require('body-parser'),
-    cors        = require('cors');
+const express     = require('express'),
+      app         = express(),
+      path        = require('path'),
+      http        = require('http'),
+      bodyParser  = require('body-parser'),
+      cors        = require('cors'),
+      port        = process.env.PORT || 8080;
 
-const app = express();
-const port = process.env.PORT || 4000;
-
-const server = app.listen(function () {
-  console.log('Listening on port ' + port);
-});
+app.use(express.static(path.join(__dirname, '\./dist/tr6crud/'))) //express()
+  .use(cors)
+  .options('*', cors())
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  .use(bodyParser.json({
+    'type': 'application/javascript'
+  }))
+  .use(bodyParser.json())
+  .use(express.static(path.join(__dirname, '\./dist/tr6crud/index.html')))
+  .get('\/*', (req, res) => {
+    console.log('path=' + path.join(__dirname, 'dist/tr6crud/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/tr6crud/index.html'))
+  })
+  .listen(port, () => {
+    console.log('path=' + path.join(__dirname, '\/dist/tr6crud/index.html'));
+    console.log('Listening on port ' + port);
+  })
+  module.exports = express;
